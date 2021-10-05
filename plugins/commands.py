7112d00cacd9bfd5e115fb438f55e22b20355207
devil982 <85802649+devil982@users.noreply.github.com>
2021-10-05 21:46:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,7 +99,8 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        
+                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
