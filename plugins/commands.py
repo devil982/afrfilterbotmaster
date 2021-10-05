6e8342a0cb756e6f0979c5cb95d81bf2619abee8
devil982 <85802649+devil982@users.noreply.github.com>
2021-10-05 23:16:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("join for more movies", url="https://t.me/cinemgraamam_requestgroup")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
