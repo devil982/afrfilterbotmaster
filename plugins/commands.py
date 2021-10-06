ff327af691f3eeeeff90f7688a780837136df6b5
devil982 <85802649+devil982@users.noreply.github.com>
2021-10-06 11:55:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,7 +97,7 @@ async def start(bot, cmd):
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [
+                ],[
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
                         InlineKeyboardButton("join for more movies", url="https://t.me/cinemgraamam_requestgroup"),
