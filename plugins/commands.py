4287cee10c3a65713b7dfea406fbff5c95a506c4
devil982 <85802649+devil982@users.noreply.github.com>
2021-10-08 20:26:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -205,4 +205,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Developer : <a href='https://t.me/cg_suggestionbot'>contact me</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nMain Channel : <a href='https://t.me/ott_releases_only'>new releases</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
