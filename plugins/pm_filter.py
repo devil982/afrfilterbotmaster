c78d5c3d1345358cf827bdb63246f7addaefe75d
thomas shelby <85802649+devil982@users.noreply.github.com>
2021-10-21 12:49:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -68,6 +68,7 @@ async def filter(client, message):
                     ]
                 ),
                 parse_mode="markdown"
+             reply_to_message_id=message.message_id                         
             )
             return
 
